2d6ef894ebb1fe8852591eb577234acb559bfe9d
MAAQ90 <134720024+MAAQ90@users.noreply.github.com>
2023-11-28 21:52:42 +0000

    M2_T8: Updates Code

--- database_utils.py
+++ database_utils.py
@@ -16,22 +16,22 @@ from sqlalchemy import inspect
 class DatabaseConnector:
 
   # contruct class
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def read_db_creds(self, filename):
-    with open(filename, "r") as db_creds:
-      return yaml.safe_load(db_creds)
+    def read_db_creds(self, filename):
+        with open(filename, "r") as db_creds:
+            return yaml.safe_load(db_creds)
 
-  def init_db_engine(self, cred):
-    DATABASE_TYPE = 'postgresql'
-    DBAPI = 'psycopg2'
-    engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{cred['RDS_USER']}:{cred['RDS_PASSWORD']}@{cred['RDS_HOST']}:{cred['RDS_PORT']}/{cred['RDS_DATABASE']}")
-    return engine
+    def init_db_engine(self, cred):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        engine = create_engine(f"{'postgresql'}+{'psycopg2'}://{cred['RDS_USER']}:{cred['RDS_PASSWORD']}@{cred['RDS_HOST']}:{cred['RDS_PORT']}/{cred['RDS_DATABASE']}")
+        return engine
 
-  def list_db_tables(self, engine):
-    inspector = inspect(engine)
-    return inspector.get_table_names()
+    def list_db_tables(self, engine):
+        inspector = inspect(engine)
+        return inspector.get_table_names()
 
-  def upload_to_db(self, df, table_name, engine):
-    df.to_sql(table_name, engine, if_exists='replace')
+    def upload_to_db(self, df, table_name, engine):
+        df.to_sql(table_name, engine, if_exists='replace', index=False)
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import database_utils
 import data_extraction
 import data_cleaning
-
-
-# In[2]:
-
 
 # **************
 # INITIALISATION
@@ -20,80 +13,314 @@ my_instance 	= database_utils.DatabaseConnector()
 my_data_extract = data_extraction.DataExtractor()
 my_data_clean   = data_cleaning.DataCleaning()
 
+rds_cred		= my_instance.read_db_creds('rds_creds.yaml')
+mydb_cred = my_instance.read_db_creds('mydb_creds.yaml')
 
-db_cred		= my_instance.read_db_creds('db_creds.yaml')
-db_engine 	= my_instance.init_db_engine(db_cred)
-table_name 	= my_instance.list_db_tables(db_engine)
+'''
+# *************
+# T3: USER DATA
+# *************
 
-print(f'table_name: {table_name}')
+print(f'\nPROCESSING: USER DATA\n')
 
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
 
-# In[ ]:
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
 
+print('extracting data')
+table_name = my_instance.list_db_tables(db_engine)
+#print(f'table_name: {table_name}')
+df_userdata = my_data_extract.read_rds_table(db_engine, table_name[1])
 
-# *********
-# USER DATA
-# *********
+print('closing remote engine')
+db_engine.dispose()
 
-# EXTRACT
+#print(df_userdata.head())
 
-df_userdata	= my_data_extract.read_rds_table(db_engine, table_name[1])
-df_userdata.head()
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
 
-# CLEAN
+print('cleaning extracted data')
+df_userdata = my_data_clean.clean_user_data(df_userdata)
 
-df_userdata	= my_data_clean.clean_user_data(df_userdata)
+#print(df_userdata.head())
 
-# OUTPUT
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
 
-df_userdata.head()
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
+
+print('uploading to local database')
 my_instance.upload_to_db(df_userdata, 'dim_users', db_engine)
 
+print('closing local engine')
+db_engine.dispose()
 
-# In[ ]:
+#exit()
 
+# *************
+# T4: CARD DATA 
+# *************
 
-# *********
-# CARD DATA 
-# *********
+print('\nPROCESSING: CARD DATA\n')
 
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
+
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
+
+print('extracting card data')
 df_carddata	= my_data_extract.retrieve_pdf_data("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf")
-print(df_carddata.head())
+print(df_carddata.info())
+print(df_carddata.describe())
 
-# T4_S3 CLEAN df_carddata
+print('closing remote engine')
+db_engine.dispose()
+
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
+
+print('cleaning extracted data')
+df_carddata = my_data_clean.clean_card_data(df_carddata)
+
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
+
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
+
+print('uploading to local database')
+my_instance.upload_to_db(df_carddata, 'dim_card_details', db_engine)
+
+print('closing local engine')
+db_engine.dispose()
+
+print(df_carddata.info())
+print(df_carddata.describe())
+
+#exit()
 
 
 
-# In[ ]:
+# ***************
+# T5: STORES DATA
+# ***************
 
+print('\nPROCESSING: STORES DATA\n')
 
-# ***********
-# STORES DATA
-# ***********
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
 
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
+
+print('extracting stores data')
 number_of_stores = my_data_extract.list_number_of_stores()
 print(f'number of stores:{number_of_stores}')
 
-# T5_S3 DF storedata
+df_storedata = my_data_extract.retrieve_stores_data(number_of_stores)
+print(df_storedata.info())
+print(df_storedata.describe())
 
-# T5_S4 CLEAN df_storedata
+print('closing remote engine')
+db_engine.dispose()
 
-# T5_S4 UPLOAD df_storedata in dim_store_details
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
 
+print('cleaning extracted data')
+df_storedata = my_data_clean.clean_store_data(df_storedata)
 
-# In[ ]:
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
 
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
 
-# *************
-# PRODUCTS DATA
-# *************
+print('uploading to local database')
+my_instance.upload_to_db(df_storedata, 'dim_store_details', db_engine)
 
-df_productsdata = my_data_ext.extract_from_s3()
-print(df_productsdata)
+print('closing local engine')
+db_engine.dispose()
+
+'''
+
+# *****************
+# T6: PRODUCTS DATA
+# *****************
+'''
+print(f'\nPROCESSING: PRODUCT DETAILS\n')
+
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
+
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
+
+print('extracting data')
+df_productdetails = my_data_extract.extract_from_s3()
+print(df_productdetails)
+
+print('closing remote engine')
+db_engine.dispose()
+
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
 
 # T6_S2 CLEAN convert_product_weight
 
+print('cleaning extracted data')
+
 # T6_S3 CLEAN clean_products_data
 
-# T6_S4 UPLOAD sales_data using upload_to_db
+df_productdetails = my_data_clean.convert_product_weights(df_productdetails, 'weight')
+df_productdetails = my_data_clean.clean_products_data(df_productdetails)
+print(df_productdetails)
 
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
+
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
+
+print('uploading to local database')
+my_instance.upload_to_db(df_productdetails, 'dim_products', db_engine)
+
+print('closing local engine')
+db_engine.dispose()
+'''
+
+
+# **********
+# T7: ORDERS
+# **********
+'''
+print(f'\nPROCESSING: ORDERS DATA\n')
+
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
+
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
+
+print('extracting data')
+table_name = my_instance.list_db_tables(db_engine)
+#print(f'table_name: {table_name}')
+df_ordersdata = my_data_extract.read_rds_table(db_engine, table_name[2])
+
+print('closing remote engine')
+db_engine.dispose()
+
+print(df_ordersdata.head())
+print(df_ordersdata.tail())
+print('******************************************************************')
+print(df_ordersdata.info())
+print('******************************************************************')
+print(df_ordersdata.describe())
+print('******************************************************************')
+
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
+
+print('cleaning extracted data')
+
+df_ordersdata = my_data_clean.clean_orders_data(df_ordersdata)
+print(df_ordersdata)
+
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
+
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
+
+print('uploading to local database')
+my_instance.upload_to_db(df_ordersdata, 'orders_table', db_engine)
+
+print('closing local engine')
+db_engine.dispose()
+'''
+
+
+# ***************
+# T7: EVENTS DATA
+# ***************
+
+# --------------------------
+# EXTRACT FROM REMOTE SOURCE
+# --------------------------
+
+print('initiating remote engine')
+db_engine = my_instance.init_db_engine(rds_cred)
+db_engine.connect()
+
+print('extracting data')
+df_datesdata = my_data_extract.extract_dates_data()
+
+print('closing remote engine')
+db_engine.dispose()
+
+print(df_datesdata)
+
+print('******************************************************************')
+print(df_datesdata.info())
+print('******************************************************************')
+print(df_datesdata.describe())
+print('******************************************************************')
+
+# --------------------
+# CLEAN EXTRACTED DATA
+# --------------------
+
+print('cleaning extracted data')
+
+df_datesdata = my_data_clean.clean_dates_data(df_datesdata)
+print(df_datesdata)
+
+print('******************************************************************')
+print(df_datesdata.info())
+print('******************************************************************')
+print(df_datesdata.describe())
+print('******************************************************************')
+
+# ------------------------
+# UPLOAD TO LOCAL DATABASE
+# ------------------------
+
+print('initiating local engine')
+db_engine = my_instance.init_db_engine(mydb_cred)
+db_engine.connect()
+
+print('uploading to local database')
+my_instance.upload_to_db(df_datesdata, 'dim_date_times', db_engine)
+
+print('closing local engine')
+db_engine.dispose()
